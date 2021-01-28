6015474252868479500c0c0f8585b635ddde4c56
Cristian <desancheztorres@gmail.com>
2021-01-28 12:04:16 +0000

    Actually building the login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller {
 
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
         if(!auth()->attempt($request->only('email', 'password'))) {
             throw new AuthenticationException();
